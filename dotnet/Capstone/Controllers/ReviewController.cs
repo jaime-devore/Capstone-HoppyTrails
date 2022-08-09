4129d2cdfbeb7c76190b28600e8d01387f708683
Dante Amico <danteamicolighting@gmail.com>
2022-08-09 12:30:54 -0400

    Review Controller v1

---
+++ dotnet/Capstone/Controllers/ReviewController.cs
@@ -0,0 +1,39 @@
+using system;
+using system.collections.generic;
+using system.linq;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Capstone.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private IReviewDao reviewDao;
+
+        public BreweryController(IReviewDao reviewDao)
+        {
+            this.reviewDao = reviewDao;
+        }
+        
+        [HttpGet("/routes/reviews")]
+        public ActionResult<List<Review>> AllReviews()
+        {
+            return Ok(reviewDao.GetAllReviews());
+        }
+
+        [HttpGet("/routes/{id}/reviews")]
+        public ActionResult<List<Review>> ReviewsByRouteId(int id)
+        {
+            return Ok(reviewDao.GetReviews(id));
+        }
+
+        [HttpGet("/routes/reviews/{reviewId}")]
+        public ActionResult<Review> SingleReviewById(int reviewId)
+        {
+            return Ok(reviewDao.GetReviews(reviewId));
+        }
+
+    }
+}
