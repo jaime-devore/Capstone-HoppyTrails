6d770133c3d55e0991b6be9191bac68af33c70a5
Jaime DeVore <devore.jaime@gmail.com>
2022-08-09 12:29:56 -0400

    BreweryDAO and IDAO complete for get one/all Breweries. Corrections to Brewery Model

--- dotnet/Capstone/DAO/IBreweryDAO.cs
+++ dotnet/Capstone/DAO/IBreweryDAO.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Capstone.Models;
 
 namespace Capstone.DAO
 {
-    interface IBreweryDAO
+    public interface IBreweryDAO
     {
+        //method to return all breweries
+        List<Brewery> GetAllBreweries();
+
+        //method to get a single brewery by Brewery ID
+        Brewery GetBreweryByBreweryID(int breweryID);
+
+
     }
 }
--- dotnet/Capstone/Models/Brewery.cs
+++ dotnet/Capstone/Models/Brewery.cs
@@ -12,10 +12,11 @@ namespace Capstone.Models
         public string PhoneNumber { get; set; }
         public string About { get; set; }
         public string WebSite { get; set; }
+        public string Address { get; set; }
         public string Image { get; set; }
         public string Hours { get; set; }
         public string City { get; set; }
         public string State { get; set; }
-        public int ZipCode { get; set; }
+        public string ZipCode { get; set; }
     }
 }
