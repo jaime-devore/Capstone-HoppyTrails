b59dfe9c8c31d93aaba987a6ea6c2faaf442d0d7
Robert Chumsae <robert.chumsae@gmail.com>
2022-08-09 12:00:20 -0400

    Created models for Tags, Reviews, And Breweries

---
+++ dotnet/Capstone/Models/Brewery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Brewery
+    {
+        public int BreweryId { get; set; }
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+        public string About { get; set; }
+        public string WebSite { get; set; }
+        public string Image { get; set; }
+        public string Hours { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
