71b00cfc6205cb6e02791e8766e8931263605c52
Robert Chumsae <robert.chumsae@gmail.com>
2022-08-10 11:43:57 -0400

    Added Beer model

---
+++ dotnet/Capstone/Models/Beer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Capstone.Models
+{
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public decimal ABV { get; set; }
+
+    }
+}
