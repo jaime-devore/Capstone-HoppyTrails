86eb8d105e0ca2f601fe123133ac1eff49b2cf44
David <d2.hewlett@gmail.com>
2022-08-12 11:34:00 -0400

    Flippy Cards mostly work

--- vue/src/views/AllBreweries.vue
+++ vue/src/views/AllBreweries.vue
@@ -2,37 +2,56 @@
   <div class="view-root row p-1">
     <h1>List Of All Breweries</h1>
 
-<div class="flip-card">
+<div class="flip-card col-lg-3" v-for="brewery in allBreweries" v-bind:key="brewery.id">
   <div class="flip-card-inner">
     <div class="flip-card-front">
-      <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+      
+        <!-- *********** FRONTCARD *********** -->
+            <div class="">
+                    <div class="card border border-secondary w-100 p-2" >
+                        <div class="card-header  bg-success text-white ">
+                            <h5 class="card-title">{{brewery.name}}</h5>
+                        </div>
+                        <div class="card-body">
+                            <img class="img-thumbnail" v-bind:src="brewery.image" />
+                            
+                            <p><span class="fw-bold" >PHONE: </span>{{brewery.phoneNumber}}</p>
+                            <p><span class="fw-bold" >ADDRESS:</span> {{brewery.address}}</p>
+                            
+                        </div>
+                    </div>
+                </div><!-- end of BS card -->
+
+
+      
     </div>
     <div class="flip-card-back">
-      <h1>John Doe</h1> 
-      <p>Architect & Engineer</p> 
-      <p>We love that guy</p>
+      
+
+            <div class="bckCard">
+                    <div class="card border border-secondary w-100 p-2" >
+                        <div class="card-header ">
+                            <h5 class="card-title"> {{brewery.name}}</h5>
+                        </div>
+                        <div class="card-body">
+                             <h4 class="fw-bold" > ABOUT</h4>
+                            <p class="card-text mh-25 ">{{brewery.about}}</p>
+                            
+                        </div>
+                    </div>
+                </div>
+
+
+
+
+
+
     </div>
   </div>
 </div>
 
 
-    <div class="col-lg-3" v-for="(brewery, index) in allBreweries" v-bind:key="brewery.id">
-        <div class="card border border-secondary w-100 p-2" >
-            <div class="card-header  bg-success text-white ">
-                <h5 class="card-title"> {{index}} - {{brewery.name}}</h5>
-            </div>
-            <div class="card-body">
-                <img class="img-thumbnail" v-bind:src="brewery.image" />
-                
-                <p><span class="fw-bold" >PHONE: </span>{{brewery.phoneNumber}}</p>
-                <p><span class="fw-bold" >WEBSITE: </span>{{brewery.webSite}}</p>
-                <p><span class="fw-bold" >ADDRESS:</span> {{brewery.address}}</p>
-                <h4 class="fw-bold" > ABOUT</h4>
-                <p class="card-text">{{brewery.about}}</p>
-                
-            </div>
-        </div>
-    </div>
+    
   </div>
 </template>
 
@@ -72,14 +91,14 @@ export default {
 .flip-card {
   background-color: transparent;
   width: 300px;
-  height: 300px;
+  height: 400px;
   perspective: 1000px;
 }
 
 .flip-card-inner {
   position: relative;
   width: 100%;
-  height: 100%;
+  height: 400px;
   text-align: center;
   transition: transform 0.6s;
   transform-style: preserve-3d;
@@ -93,19 +112,22 @@ export default {
 .flip-card-front, .flip-card-back {
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: 300px;
   -webkit-backface-visibility: hidden;
   backface-visibility: hidden;
 }
 
 .flip-card-front {
-  background-color: #bbb;
-  color: black;
+  /* background-color: #bbb; */
+  color: black; 
 }
 
 .flip-card-back {
-  background-color: #2980b9;
-  color: white;
+  /* background-color: #2980b9;
+  color: white; */
   transform: rotateY(180deg);
+}
+.bckCard{
+  height: 400px;
 }
 </style>
