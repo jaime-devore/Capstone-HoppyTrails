0d45112bc9b1734e7a0170a8305e28620c6842ed
Jaime DeVore <devore.jaime@gmail.com>
2022-08-15 09:19:37 -0400

    VUE: Trail Details Page logic built in. Went with a poop brown theme for testing. Added Trail Objects to the store with unique IDs and Brewery arrays.

--- vue/src/views/TrailDetails.vue
+++ vue/src/views/TrailDetails.vue
@@ -10,6 +10,7 @@ import TrailDetails from '../components/TrailDetails.vue'
 
 export default {
   components: { TrailDetails },
+  //this is where you would match the route.params.trailID to the this.store.state.trails.id so that the trail-details component can take it as a prop upon creation
 
 
 }
